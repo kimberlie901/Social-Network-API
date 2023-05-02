b125c74a7ab63f3944bb98dbd26366c573763b91
Kimmy <kimberlieguillaume@gmail.com>
2023-05-02 11:20:52 -0400

    updating thoughts routes

---
+++ routes/api/thoughts-routes.js
@@ -0,0 +1,31 @@
+// require express router
+const router = require("express").Router();
+
+// require the thought controller
+const { 
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThoughtById,
+    deleteThoughtById,
+    addReaction,
+    deleteReaction
+} = require("../../controllers/thought-Controller");
+
+
+// set up GET all thoughts and POST at /api/thoughts
+
+router.route("/").get(getAllThoughts).post(createThought);
+
+// set up GET one, PUT, and DELETE at /api/thoughts/:id
+
+router.route("/:id").get(getThoughtById).put(updateThoughtById).delete(deleteThoughtById);
+
+
+// set up POST and DELETE at /api/thoughts/:thoughtId/reactions
+
+router.route("/:thoughtId/reactions").post(addReaction).delete(deleteReaction);
+
+
+// export module router
+module.exports = router;
