787f9b0e3ff30b2470068f96195bf83cae141cde
Kimmy <kimberlieguillaume@gmail.com>
2023-05-01 20:00:00 -0400

    added get thoughts by id

--- controllers/thought-Controller.js
+++ controllers/thought-Controller.js
@@ -1,9 +1,12 @@
 // require thought models
 const { Thought } = require('../models');
 
+
 // Thought controller
+
+
+// create thoughts and push the thought to user's thoughts array
 const thoughtController = {
-    // create thoughts and push the thought to user's thoughts array
     createThought({ params, body }, res) {
         Thought.create(body)
             .then(({ _id }) => {
@@ -14,13 +17,52 @@ const thoughtController = {
                     { new: true }
                 );
             })
-            .then(thoughtID => {
-                if (!thoughtID) {
+            .then(thoughtData => {
+                if (!thoughtData) {
                     res.status(404).json({ message: 'No thought found with this id.' });
                     return;
                 }
-                res.json(thoughtID);
+                res.json(thoughtData);
             })
             .catch(err => res.json(err));
     }
 };
+
+
+// get all thoughts and sort by newest thought
+const getAllThoughts = (req, res) => {
+    Thought.find({})
+        .populate({
+            path: 'reactions',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(thoughtData => res.json(thoughtData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+}
+
+
+// get thought by id
+const getThoughtById = ({ params }, res) => {
+    Thought.findOne({ _id: params.id })
+        .populate({
+            path: 'reactions',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(thoughtData => {
+            if (!thoughtData) {
+                res.status(404).json({ message: 'No thought found with this id.' });
+                return;
+            }
+            res.json(thoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+}
